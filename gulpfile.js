13755bb99b51d8bee7a0e74bfef97c4c87244f96
edvin <edvinvidmar@hotmail.com>
2015-07-08 15:45:10 +0200

    Added gulp integration.

---
+++ gulpfile.js
@@ -0,0 +1,9 @@
+var gulp = require('gulp');
+var mocha = require('gulp-mocha');
+
+gulp.task('test', function () {
+  console.log("Hi from GULP!");
+	return gulp.src('./test/*.js', { read: false })
+	// gulp-mocha needs filepaths so you can't have any plugins before it
+		.pipe(mocha());
+});
